40d6c8b0259bd254928df957b334f7a3366b855e
EduardoAZA <Eduardoaffonsoz@hotmail.com>
2024-05-25 18:59:04 -0300

    Adding private routes

--- src/main.jsx
+++ src/main.jsx
@@ -4,58 +4,19 @@ import "./index.css";
 import { Toaster } from "sonner";
 import App from "./App";
 
-import Home from "./routes/home";
-import Profile from "./routes/profile";
-import Login from "./routes/login";
-import Signup from "./routes/signup";
-import ErrorPage from "./routes/ErrorPage";
-import CreateGroup from "./routes/CreateGroup";
-import MyGroups from "./routes/MyGroups";
+import Home from "./pages/home";
+import Profile from "./pages/profile";
+import Login from "./pages/login";
+import Signup from "./pages/signup";
+import ErrorPage from "./pages/ErrorPage";
+import CreateGroup from "./pages/CreateGroup";
+import MyGroups from "./pages/MyGroups";
 
-// 1 - Router configuration
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-const router = createBrowserRouter([
-  {
-    path: "/home/:id?",
-    element: <Home />
-  },
-  {
-    path: "/perfil/:id",
-    element: <Profile />
-  },
-  {
-    path: "/login",
-    element: <Login />
-  },
-  {
-    path: "/cadastro",
-    element: <Signup />
-  },
-  {
-    path: "/criar-grupo",
-    element: <CreateGroup />
-  },
-  {
-    path: "/meus-grupos",
-    element: <Signup />
-  },
-  {
-    path: "/entrar"
-  },
-  {
-    path: "*",
-    element: <ErrorPage />
-  },
-  {
-    path: "/meus-grupos/:id",
-    element: <MyGroups/>
-  }
-])
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App/>
     <Toaster position="top-right" richColors expand={true} />
   </React.StrictMode>
 );
